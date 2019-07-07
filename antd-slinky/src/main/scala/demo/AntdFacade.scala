trepidacious/ScalablyTypedDemos

Added Notification example

antd-slinky/src/main/scala/demo/AntdFacade.scala
@@ -52,6 +52,8 @@ object AntdFacade {
   type InputProps = typings.antdLib.esInputInputMod.InputProps
   @inline def ModalProps = typings.antdLib.esModalModalMod.ModalProps
   type ModalProps = typings.antdLib.esModalModalMod.ModalProps
+  @inline def NotificationArgsProps = typings.antdLib.esNotificationMod.ArgsProps
+  type NotificationArgsProps = typings.antdLib.esModalModalMod.ModalProps
   @inline def OptionProps = typings.antdLib.esSelectMod.OptionProps
   type OptionProps = typings.antdLib.esSelectMod.OptionProps
   @inline def PasswordProps = typings.antdLib.esInputPasswordMod.PasswordProps

ScalablyTyped/Demos

antd-slinky/src/main/scala/demo/AntdFacade.scala
@@ -52,6 +52,8 @@ object AntdFacade {
   type InputProps = typings.antdLib.esInputInputMod.InputProps
   @inline def ModalProps = typings.antdLib.esModalModalMod.ModalProps
   type ModalProps = typings.antdLib.esModalModalMod.ModalProps
+  @inline def NotificationArgsProps = typings.antdLib.esNotificationMod.ArgsProps
+  type NotificationArgsProps = typings.antdLib.esNotificationMod.ArgsProps
   @inline def OptionProps = typings.antdLib.esSelectMod.OptionProps
   type OptionProps = typings.antdLib.esSelectMod.OptionProps
   @inline def PasswordProps = typings.antdLib.esInputPasswordMod.PasswordProps